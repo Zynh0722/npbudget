3e58051e415a8a624ee4f47e9756f994ebc3f5a1
Zynh0722 <Zynh0722@gmail.com>
2022-09-25 22:55:21 -0700

    noqa again

--- npbudget/settings.py
+++ npbudget/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)^qi6_9c)=h31$w6+o$ekh$-mlby^&2h$tjj+nhb-i1zjxd6)k'
+SECRET_KEY = 'django-insecure-)^qi6_9c)=h31$w6+o$ekh$-mlby^&2h$tjj+nhb-i1zjxd6)k' # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
