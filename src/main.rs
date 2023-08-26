3785451052b4deeeb04b0db67a8eefaa0ebe76b1
Zynh0722 <Zynh0722@gmail.com>
2023-08-25 23:21:22 -0700

    ructe has joined the party!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+include!(concat!(env!("OUT_DIR"), "/templates.rs"));
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf = Vec::new();
+    templates::hello_args_html(&mut buf, "World").unwrap();
+    println!("{}", String::from_utf8(buf).unwrap());
 }
